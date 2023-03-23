22467671d535fd98da8892e289821d1623a0cab3
Master's Of Coding <128671244+MastersOfCoding2908@users.noreply.github.com>
2023-03-23 15:09:37 +0530

    Add files via upload

---
+++ TREE_PATTERN_PROGRAM/main.py
@@ -0,0 +1,21 @@
+# A Python Program To Make A Tree Pattern From Stars
+# Let' s Start Coding With Master's Of Coding
+
+i = 1
+m = 1
+#For Drawing Leaves Pattern
+while i != 5:
+    i += 1
+    a = 5 - i
+    b = 5 + i
+    c = 8 - i
+    print(" "*a,"*"*(b-c))
+# For Drawing Lower Pattern
+while m != 4:
+    m = m+1
+    print("  ","*"*3)
+print("\t\tTree Pattern !")
+print("\t\tDon't Forget To Subscribe My Channel.")
+print("\t\tThanks For Watching This Video !")
+# You Can Find The Source Code From The Description With Explanation.
+# Thank You
